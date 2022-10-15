9aa77d492c3d7b5f868991c686571295920252ad
Cherry Pham <meokkmun@gmail.com>
2022-10-15 14:48:33 -0400

    Add walls

--- solar_house_sim.m
+++ solar_house_sim.m
@@ -1,46 +1,46 @@
-function [Time,T] = solar_house_sim()
+function [Time,T] = solar_house_sim(w_thickness, esu_thickness)
     % HOUSE CONSTRUCTION PARAMETERS
-    B_HEIGHT = 3.2; % Wall Height (m)
-    b_depth = 2.2; % Building Depth (m) - calculated
-    B_LENGTH = 5.1; % Building Length (m)
+    W_HEIGHT = 3.2; % Wall Height (m)
+    W_DEPTH = 5; % Building Depth (m) - calculated
+    W_LENGTH = 5.1; % Building Length (m)
     TOP_PANE_HEIGHT = 0.4;
     BOTTOM_PANE_HEIGHT = 0.2;
-    SA_PANES = TOP_PANE_HEIGHT * b_depth + BOTTOM_PANE_HEIGHT * b_depth;
+    SA_PANES = TOP_PANE_HEIGHT * W_DEPTH + BOTTOM_PANE_HEIGHT * W_DEPTH;
     HEIGHT_WINDOW = 2.6; % Window Height (m)
-    b_thickness = 0.02; % Wall thickness (m) - sweeping through
-    esu_thickness = 0.5; % Storage unit thickness (m) - sweeping through
+%     w_thickness = 0.02; % Wall thickness (m) - sweeping through
+%     esu_thickness = 0.5; % Storage unit thickness (m) - sweeping through
     
     % THERMAL-ODE-RELATED PARAMETERS
-    k_walls = 0.04; % Conductivity of fiberglass insulation (W/m-K)
-    p_esu = 3000; % Energy Storage Unit (ESU) Density (kg/m^3)
-    c_esu = 800;  % ESU 
-    h_indoors = 15;
-    h_outdoors = 30;
-    T_air = -3;
+    K_WALLS = 0.04; % Conductivity of fiberglass insulation (W/m-K)
+    P_ESU = 3000; % Energy Storage Unit (ESU) Density (kg/m^3)
+    C_ESU = 800;  % ESU 
+    H_INDOORS = 15;
+    H_OUTDOORS = 30;
+    T_AIR = -3;
     T_storage_i = 20; % Randomly picking from given range of ()
-    h_window = 1.4;
+    H_WINDOW = 1.4;
 
-    v_esu = esu_thickness * b_depth * B_LENGTH;
-    C = p_esu * v_esu * c_esu;
+    v_esu = esu_thickness * W_DEPTH * W_LENGTH;
+    C = P_ESU * v_esu * C_ESU;
 
     % CALCULATING AREAS
-    SA_esu = (B_LENGTH - 2 * b_thickness) * b_depth;
-    SA_walls_in = 2*((B_LENGTH - 2 * b_thickness) * b_depth) + B_HEIGHT * b_depth + SA_PANES;
-    SA_walls_out = 2 * B_LENGTH * b_depth + B_HEIGHT * b_depth + SA_PANES + 4 * b_thickness;
-    SA_window = HEIGHT_WINDOW * b_depth;
+    SA_esu = (W_LENGTH - 2 * w_thickness) * W_DEPTH;
+    SA_walls_in = 2*((W_LENGTH - 2 * w_thickness) * W_DEPTH) + W_HEIGHT * W_DEPTH + SA_PANES + 2*((W_LENGTH - 2 * w_thickness) * W_HEIGHT);
+    SA_walls_out = 2 * W_LENGTH * W_DEPTH + W_HEIGHT * W_DEPTH + SA_PANES + 2 * W_LENGTH * W_HEIGHT;
+    SA_window = HEIGHT_WINDOW * W_DEPTH;
     
     % ODE SUBEQUATIONS
     % Resistance
-    R_air_in = 1./(h_indoors * SA_esu);
-    R_floor_cond = b_thickness./(k_walls * SA_walls_in);
-    R_floor_conv = 1./(h_indoors * SA_walls_in);
-    R_window = 1./(h_window * SA_window);
-    R_air_out = 1./(h_outdoors * SA_walls_out);
+    R_air_in = 1./(H_INDOORS * SA_esu);
+    R_floor_cond = w_thickness./(K_WALLS * SA_walls_in);
+    R_floor_conv = 1./(H_INDOORS * SA_walls_in);
+    R_window = 1./(H_WINDOW * SA_window);
+    R_air_out = 1./(H_OUTDOORS * SA_walls_out);
     R = R_air_in + 1./(1./(R_floor_cond + R_floor_conv) + 1./(R_window)) + R_air_out;
     
     % Time values in hours
     t_i = 0;
-    t_f = 365.25 * 24 * 3600;
+    t_f = 30 * 24 * 3600;
     
     % Initial Temperature
     
@@ -52,7 +52,7 @@ function [Time,T] = solar_house_sim()
         % Solar flux
         q = -1 * cos((pi * t) / (12 * 3600)) + 224 * cos((pi * t) / (6 * 3600)) + 210;
         q_in = q * SA_window;
-        q_out = (T_storage_i - T_air) / R;
+        q_out = (T_storage_i - T_AIR) / R;
         dTdt = q_in/C- q_out/C;
         res = dTdt;
     end
